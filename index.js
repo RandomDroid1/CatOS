5b1cd27ccb8edfdad4dacd3c4184b45a8804fcc5
RandomDroid1 <lilspi321@gmail.com>
2026-06-16 12:22:16 -0700

    added some css & js to the login page, right now I'm trying to make it look good & kind of old before moving along to the landing page

--- index.js
+++ index.js
@@ -3,5 +3,23 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 
 function LoginButton() {
-    document.getelement
+    var Username = document.getElementById("UsernameBox").value
+    var Password = document.getElementById("PasswordBox").value
+    if (Username == "MaggieSmith" && Password == "SEAPORT") {
+        LoginSuccessSequence()
+    }
+    if (Username != "MaggieSmith" || Password != "SEAPORT") {
+        LoginFailureSequence()
+    }
+
 }   
+
+
+function LoginSuccessSequence() {
+
+}
+
+function LoginFailureSequence() {
+    console.log("Failure")
+    document.getElementById("LoginButtonID").style.top = "70%"
+}
